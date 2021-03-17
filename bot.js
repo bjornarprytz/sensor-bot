579b7de7c28097ee9b75d991a58db5a5c7386c5a
bjornarprytz <bjornarprytz@users.noreply.github.com>
2021-03-17 15:31:06 +0100

    parsing commands and updated eslint

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+const { prefix, token } = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,10 +9,30 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-	if (msg.content === 'ping') {
-		msg.reply('pong');
-	}
-});
+client.on('message', onMessage);
 
-client.login('token');
+client.login(token);
+
+
+function onMessage(message) {
+	if (!message.content.startsWith(prefix)) {
+		return;
+	}
+
+	const words = message.content.substring(prefix.length).split(' ');
+
+	if (words.length == 0) {
+		return;
+	}
+
+	const command = words[0];
+	const args = words.slice(1);
+
+	switch(command) {
+	case 'ping':
+		message.reply(args.join(' '));
+		break;
+	default:
+		message.reply(`Unrecognized command: ${command}`);
+	}
+}
