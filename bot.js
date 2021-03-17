0c3a50c1c0cbb8806a3c78b87d2f8b1e872a8e55
bjornarprytz <bjornarprytz@users.noreply.github.com>
2021-03-17 11:09:26 +0100

    praise be eslint

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
-"use strict"
+'use strict';
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+	if (msg.content === 'ping') {
+		msg.reply('pong');
+	}
 });
 
 client.login('token');
