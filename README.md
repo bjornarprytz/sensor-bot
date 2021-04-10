f5c42e21529ad245820497e75789bad3faf92ef9
bjornarprytz <bjornarprytz@users.noreply.github.com>
2021-04-10 23:50:56 +0200

    Added event handlers from the discord guide.

--- README.md
+++ README.md
@@ -22,3 +22,15 @@ Add file in `/commands/<category>/<command_name.js>` [example](commands/fun/hell
 - cooldown: int (seconds, per user, per alias; **Default: 3**)
 - guildOnly: bool (true => command only works in server channels (not DMs))
 - permissions: [discord permissions](https://discordjs.guide/command-handling/adding-features.html#command-permissions)
+
+## Adding Events
+
+Add file in `/events/client/<event_name.js>` [example](events/client/hello.js)
+
+[Client events documentation](https://discord.js.org/#/docs/main/stable/class/Client).
+
+### Event Options
+
+- name: must match the desired event name in [Client.Events](https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e-channelCreate).
+- once: bool (true => only runs once)
+- execute: event handler function. The client itself will be passed to the event handler as the last parameter.
--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const fs = require('fs');
-const { prefix, token } = require('./config.json');
+const { token } = require('./config.json');
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
@@ -19,76 +19,17 @@ for (const folder of commandFolders) {
 	}
 }
 
-client.on('ready', () => {
-	console.log(`Logged in as ${client.user.tag}!`);
-});
+const eventFiles = fs.readdirSync('./events/client').filter(file => file.endsWith('.js'));
 
-client.on('message', onMessage);
-
-client.login(token);
-
-
-function onMessage(message) {
-	if (!message.content.startsWith(prefix) || message.author.bot) {
-		return;
+for(const file of eventFiles) {
+	const event = require(`./events/client/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
 	}
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-				|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) return;
-
-	if (command.permissions) {
-		const authorPermissions = message.channel.permissionsFor(message.author);
-		if(!authorPermissions || authorPermissions.has(command.permissions)) {
-			return message.reply('You cannot do this!');
-		}
-	}
-
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	const { cooldowns } = client;
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-	try {
-		command.execute(message, args);
-	}
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
+	else {
+		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
+
+
+client.login(token);
